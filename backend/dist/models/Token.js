d86957e3f5366e437d68785c85102144d07f5fca
Srinivas Dharpally <srinudarpally@gmail.com>
2024-05-31 01:02:50 +0530

    Checkout

--- backend/dist/models/Token.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from "mongoose";
-const tokenSchema = new Schema({
-    token: {
-        type: String,
-        required: true,
-    },
-});
-export default model("tokens", tokenSchema);
-//# sourceMappingURL=Token.js.map
--- backend/dist/routes/auth.js
+++
@@ -1,8 +0,0 @@
-import { googleAuth, logout, tokenRefresh } from "../Controllers/authControllers.js";
-import express from "express";
-const router = express.Router();
-router.get("/", googleAuth);
-router.post("/logout", logout);
-router.post("/token", tokenRefresh);
-export default router;
-//# sourceMappingURL=auth.js.map
