7eca128750a7f017992107cf8254625a0b011e93
Srinivas Dharpally <srinudarpally@gmail.com>
2024-06-09 09:47:06 +0530

    Cros Updated

--- client/src/Interceptor/axiosInterceptor.ts
+++ client/src/Interceptor/axiosInterceptor.ts
@@ -1,71 +1,62 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import axios, { InternalAxiosRequestConfig } from "axios";
-import { url } from "../utils/baseUrl";
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import { url } from '../utils/baseUrl';
 
-const axiosInstance = axios.create({});
+const axiosInstance = axios.create({
+  baseURL: url,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+});
 
-interface CustomAxiosConfig extends InternalAxiosRequestConfig<any> {
-  headers: any;
+// Define a custom interface for AxiosRequestConfig to include headers
+interface CustomAxiosConfig extends AxiosRequestConfig {
+  headers: {
+    Authorization?: string;
+  };
 }
 
 axiosInstance.interceptors.request.use(
-
-    async (config: CustomAxiosConfig) => {
-        config.headers = {
-            Authorization: `Bearer ${JSON.parse(
-                localStorage.getItem("access_token")!
-            )}`,
-        };
-
-        return config;
-    },
-    (error) => {
-        Promise.reject(error);
+  async (config: CustomAxiosConfig) => {
+    const accessToken = localStorage.getItem('access_token');
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${JSON.parse(accessToken)}`;
     }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 axiosInstance.interceptors.response.use(
-    (response) => response,
-    async (error) => {
+  (response: AxiosResponse) => response,
+  async (error) => {
+    const originalRequest = error.config;
 
-        const originalRequest = error.config;
-        
-        
+    if (error.response.status === 401 && error.response.statusText === 'Unauthorized') {
+      const refreshToken = localStorage.getItem('refresh_token');
 
-        if(
-            error.response.status === 401 &&
-            error.response.statusText === "Unauthorized"
-        ) {
-            
-            const refreshToken = localStorage.getItem("refresh_token");
+      if (refreshToken) {
+        try {
+          const response = await axiosInstance.post(`${url}/oauth/token`, {
+            token: JSON.parse(refreshToken),
+          });
 
-            if(refreshToken) {
-                try {
-                    const response = await axiosInstance.post(`${url}/oauth/token`, {
-                        token: JSON.parse(refreshToken), 
-                    });
+          localStorage.setItem('access_token', JSON.stringify(response.data.access_token));
 
-                    localStorage.setItem(
-                        "access_token",
-                        JSON.stringify(response.data.access_token)
-                    );
+          axiosInstance.defaults.headers['Authorization'] = `Bearer ${response.data.access_token}`;
+          originalRequest.headers['Authorization'] = `Bearer ${response.data.access_token}`;
 
-                    axiosInstance.defaults.headers["Authorization"] =
-                        "Bearer " + response.data.access_token;
-                    originalRequest.headers["Authorization"] =
-                        "Bearer " + response.data.access_token;
-                return await axiosInstance(originalRequest);
-
-                }
-
-                catch(err) {
-                    console.log(err);
-                }
-            }
+          return axiosInstance(originalRequest);
+        } catch (err) {
+          console.error('Failed to refresh token:', err);
         }
-
-        return Promise.reject(error);
+      }
     }
+
+    return Promise.reject(error);
+  }
 );
 
 export const httpRequest = axiosInstance;
