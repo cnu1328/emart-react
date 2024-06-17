1fd351895fe61744a1311dbb04ff61f98a3f41a5
cnu1328 <srinudarpally@gmail.com>
2024-06-17 14:45:06 +0530

    Admin Section Added

--- client/src/ui-library/popConfirm/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-import { Button } from "ui-library/button";
-
-export const ButtonContainer = styled(Button)`
-    border: none;
-    background: transparent;
-`
